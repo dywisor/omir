66653a89d58f236dfe588bb40aeb06eded9668d5
André Erdmann <dywi@mailerd.de>
2020-03-01 22:30:21 +0100

    site: add dig-based DNS lookup

--- site/rootfs/FACTORY/site/share/netconfig.sh
+++ site/rootfs/FACTORY/site/share/netconfig.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 load_lib netconfig/util
-load_lib netconfig/lookup-ugly
+load_lib netconfig/lookup
 load_lib netconfig/ifconfig-info
 load_lib netconfig/dhclient-lease-info
 load_lib netconfig/iface-cur-config
 load_lib netconfig/iface-config-base
+load_lib netconfig/iface-config-hostname
 load_lib netconfig/iface-config-dhcp
 load_lib netconfig/iface-config-dynamic
 load_lib netconfig/iface-config-fillup
