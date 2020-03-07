48f18684dd683f4fc3e8d88610fa30a7332fdf41
André Erdmann <dywi@mailerd.de>
2020-03-07 13:52:43 +0100

    site: re-run user creation for login user
    This does not change anything for except that the login user's
    home directory gets set to mode 0700,
    but will be used to update files from /etc/skel in future.

--- site/src/rootfs/base/FACTORY/site/hooks/install.site/39-login-user.sh
+++ site/src/rootfs/base/FACTORY/site/hooks/install.site/39-login-user.sh
@@ -2,8 +2,10 @@
 [ "${OFEAT_LOGIN_USER:-0}" -eq 1 ] || exit 0
 [ -n "${OCONF_LOGIN_USER-}" ] || exit 0
 
-print_action "fixup home dir of user ${OCONF_LOGIN_USER}"
+# COULDFIX: get UID from /etc/passwd
+autodie create_user "${OCONF_LOGIN_USER}" '1000'
 autodie eval_user_funcs "${OCONF_LOGIN_USER}"
 
+print_action "fixup home dir of user ${OCONF_LOGIN_USER}"
 autodie "${OCONF_LOGIN_USER}_add_file" /etc/skel/.profile
 autodie "${OCONF_LOGIN_USER}_add_file" /etc/skel/.vimrc
