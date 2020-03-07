2049f2597eefe093614f71fc812dcacd16e96a40
André Erdmann <dywi@mailerd.de>
2020-03-07 21:10:30 +0100

    site: cleanup: optionally delay reboot

--- site/src/rootfs/base/FACTORY/site/hooks/rc.firsttime/99-cleanup-and-reboot.sh
+++ site/src/rootfs/base/FACTORY/site/hooks/rc.firsttime/99-cleanup-and-reboot.sh
@@ -35,16 +35,26 @@ reboot
 "
 }
 
+add_delayed_reboot_code() {
+
+run_cleanup_code=1
+cleanup_code="${cleanup_code}
+shutdown -r '${1:?}'
+"
+}
+
 
 [ "${OFEAT_AUTO_CLEANUP_FILES:-0}" -eq 0 ] || add_cleanup_code
 
 print_action "Cleanup"
 
 if [ "${OFEAT_AUTO_REBOOT:-0}" -eq 1 ] && [ -e "${AUTO_REBOOT_FLAG_FILE}" ]; then
-	if rm -- "${AUTO_REBOOT_FLAG_FILE}"; then
-		add_reboot_code
-	else
+	if ! rm -- "${AUTO_REBOOT_FLAG_FILE}"; then
 		print_err "Could not remove auto-reboot flag file, will not reboot."
+	elif [ "${OFEAT_AUTO_REBOOT_DELAY:-0}" -eq 1 ]; then
+		add_delayed_reboot_code "${OCONF_AUTO_REBOOT_DELAY:-+2}"
+	else
+		add_reboot_code
 	fi
 fi
 
