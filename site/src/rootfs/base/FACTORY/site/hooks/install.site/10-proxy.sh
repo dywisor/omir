074f0aae91578d192044008c7168864001e59024
André Erdmann <dywi@mailerd.de>
2020-03-18 18:43:39 +0100

    site: fix profile.d/proxy.sh: missing mkdir

--- site/src/rootfs/base/FACTORY/site/hooks/install.site/10-proxy.sh
+++ site/src/rootfs/base/FACTORY/site/hooks/install.site/10-proxy.sh
@@ -4,6 +4,7 @@ web_proxy_check_feature || exit 0
 dst='/etc/profile.d/proxy.sh'
 
 if web_proxy_check_enabled; then
+    autodie dodir "${dst%/*}"
     dofile_site "${dst}" 0644 'root:wheel' web_proxy_gen_env
 else
     autodie rm -f -- "${dst}"
