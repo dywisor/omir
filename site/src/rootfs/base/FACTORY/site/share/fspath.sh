eb23d79fc2f8c8fe1b7a4c35ef07b254569cf641
André Erdmann <dywi@mailerd.de>
2020-03-27 19:58:59 +0100

    site: add fspath function: fspath_check_safe_relpath()

--- site/src/rootfs/base/FACTORY/site/share/fspath.sh
+++ site/src/rootfs/base/FACTORY/site/share/fspath.sh
@@ -15,3 +15,13 @@ fspath_check_parent_relpath() {
 
     return 1
 }
+
+fspath_check_safe_relpath() {
+    if fspath_check_abspath "${1}"; then
+        return 1
+    elif fspath_check_parent_relpath "${1}"; then
+        return 1
+    else
+        return 0
+    fi
+}
