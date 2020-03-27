1cd229972c6c476720e3ea7703208ebbf3de2d37
André Erdmann <dywi@mailerd.de>
2020-03-27 21:51:12 +0100

    site: optionally mount a ramdisk at /dev

---
+++ site/src/rootfs/base/FACTORY/site/hooks/rc.firsttime/70-ramdisk-dev.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Cannot create device nodes while in chroot,
+# so ./MAKEDEV has to be called during rc.firsttime and not install.site.
+#
+[ "${OFEAT_RAMDISK_DEV:-0}" -eq 1 ] || exit 0
+
+skel=/skel/dev
+
+autodie mkdir -p -- "${skel}"
+autodie install -m 0555 -- /dev/MAKEDEV "${skel}/MAKEDEV"
+( cd "${skel}" && ./MAKEDEV all; ) || die "Failed to create device nodes in ${skel}"
+
+autodie fstab_add_skel_mfs "${skel}" /dev 5 -o 'rw,nosuid,noexec' -i 128
+autodie mount /dev
