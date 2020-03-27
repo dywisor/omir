f688bd81a0c02155548567caf12511fe600c4f33
André Erdmann <dywi@mailerd.de>
2020-03-27 21:59:14 +0100

    site: log about /dev ramdisk

--- site/src/rootfs/base/FACTORY/site/hooks/rc.firsttime/70-ramdisk-dev.sh
+++ site/src/rootfs/base/FACTORY/site/hooks/rc.firsttime/70-ramdisk-dev.sh
@@ -4,11 +4,14 @@
 #
 [ "${OFEAT_RAMDISK_DEV:-0}" -eq 1 ] || exit 0
 
+print_action "Prepare ramdisk at /dev"
+
 skel=/skel/dev
 
 autodie mkdir -p -- "${skel}"
 autodie install -m 0555 -- /dev/MAKEDEV "${skel}/MAKEDEV"
 ( cd "${skel}" && ./MAKEDEV all; ) || die "Failed to create device nodes in ${skel}"
 
+print_action "Mount ramdisk at /dev"
 autodie fstab_add_skel_mfs "${skel}" /dev 5 -o 'rw,nosuid,noexec' -i 128
 autodie mount /dev
