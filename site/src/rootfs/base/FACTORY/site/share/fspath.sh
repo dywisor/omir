488e75123a256bb17444d3d40790879926102e0b
André Erdmann <dywi@mailerd.de>
2020-03-27 19:56:41 +0100

    site: add fspath function: fspath_check_abspath()

--- site/src/rootfs/base/FACTORY/site/share/fspath.sh
+++ site/src/rootfs/base/FACTORY/site/share/fspath.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+fspath_check_abspath() {
+    case "${1}" in
+        /*) return 0 ;;
+    esac
+
+    return 1
+}
+
 fspath_check_parent_relpath() {
     case "${1}" in
         '..'|'../'*|*'/..'|*'/../'*) return 0 ;;
