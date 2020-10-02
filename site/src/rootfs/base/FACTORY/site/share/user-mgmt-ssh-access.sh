6683542676eb68664ebf149d8f78fc250d83ad73
André Erdmann <dywi@mailerd.de>
2020-10-02 10:07:01 +0200

    site, add function: user_set_ssh_access()

--- site/src/rootfs/base/FACTORY/site/share/user-mgmt-ssh-access.sh
+++ site/src/rootfs/base/FACTORY/site/share/user-mgmt-ssh-access.sh
@@ -1,5 +1,45 @@
 #!/bin/sh
 
+# user_set_ssh_access ( var_ns )
+#
+#   Example usage: user_set_ssh_access LOGIN_USER
+#
+user_set_ssh_access() {
+    local var_ns
+    local var_feat
+    local var_conf
+    local user_name
+    local can_login
+    local can_shell
+    local can_fwd
+
+    var_ns="${1:?}"
+    var_feat="OFEAT_${var_ns}"
+    var_conf="OCONF_${var_ns}"
+
+    eval "user_name=\"\${${var_conf}-}\""
+    [ -n "${user_name}" ] || die "user name missing in namespace ${var_ns}"
+
+    eval "can_login=\"\${${var_feat}_SSH-}\""
+
+    # group mgmt disabled by empty value?
+    [ -n "${can_login}" ] || return 0
+
+    if test "${can_login}" -eq 1 2>/dev/null; then
+        eval "can_shell=\"\${${var_feat}_SSH_SHELL:-0}\""
+        eval "can_fwd=\"\${${var_feat}_SSH_FORWARDING:-0}\""
+    else
+        can_shell=0
+        can_fwd=0
+    fi
+
+    print_info "Configuring SSH access for user ${user_name}:"
+    print_info "  login=${can_login} shell=${can_shell} forwarding=${can_fwd}"
+    autodie user_set_ssh_login "${user_name}" "${can_login}"
+    autodie user_set_ssh_shell "${user_name}" "${can_shell}"
+    autodie user_set_ssh_forwarding "${user_name}" "${can_fwd}"
+}
+
 # user_set_ssh_login ( user_name, status )
 user_set_ssh_login() {
     _user_set_ssh_access "${OCONF_SSHD_GROUP_LOGIN:?}" "${@}"
