f047061f6e5abd37bf962c657f414e6a82f2864d
André Erdmann <dywi@mailerd.de>
2020-10-02 13:13:00 +0200

    site/sshd: drop old config gen

--- site/src/rootfs/base/FACTORY/site/share/sshd/vars.sh
+++ site/src/rootfs/base/FACTORY/site/share/sshd/vars.sh
@@ -3,6 +3,5 @@
 SSHD_CONFDIR='/etc/ssh'
 SSHD_CONF_FILE="${SSHD_CONFDIR}/sshd_config"
 
-SSHD_USER_AUTH_KEYS_FILE='.ssh/authorized_keys'
 SSHD_SYSTEM_AUTH_KEYS_DIR='/etc/ssh/authorized_keys'
 SSHD_HOST_KEY_TYPES='rsa ed25519'
