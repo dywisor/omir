647ab887c955e8953b95a155a839631d1c28da06
André Erdmann <dywi@mailerd.de>
2020-10-02 11:23:34 +0200

    site/sshd: change config gen to group-based access
    Change the sshd config generator to grant access based
    on group membership (ssh-login, ssh-shell, ssh-forwarding, ...).
    
    WIP / BROKEN:
      * ssh_from for ctrl user dropped -> drop from config or reimplement
      * users do not get added to their access groups yet

---
+++ site/src/rootfs/base/FACTORY/site/share/sshd/config-gen.sh
@@ -0,0 +1,108 @@
+#!/bin/sh
+# Keep this file mostly self-sufficient - no external deps except for:
+#
+#   - vars.sh
+#   - OCONF_SSHD_GROUP_* variables:
+#      OCONF_SSHD_GROUP_LOGIN
+#      OCONF_SSHD_GROUP_SHELL
+#      OCONF_SSHD_GROUP_FORWARDING
+#
+# sshd config generator
+#
+# Call gen_sshd_config_base() to create the baseline configuration.
+# It creates a very restrictive configuration,
+# allowing neither shell login nor port forwardings.
+#
+# Access is granted based on group membership:
+#
+#  - login group (OCONF_SSHD_GROUP_LOGIN)
+#    Only users in this group may log in via SSH,
+#    regardless of any other ssh access group membership.
+#
+#  - shell group (OCONF_SSHD_GROUP_SHELL)
+#    Users in this group get a PTY.
+#
+#  - forwarding group (OCONF_SSHD_GROUP_FORWARDING)
+#    Users in this group may use tcp forwarding.
+#
+# Authorized keys are read from /etc/ssh/authorized_keys/<user_name>.
+# By default, users of the login group may read but not modify
+# their own authorized_keys file.
+#
+## Note on disabling port forwardings for most users:
+##
+##   Port forwardings are set up after successful SSH authentication,
+##   but before running ForceCommand (if configured).
+##   So, using a ForceCommand for multi-factor authentication
+##   together with AllowTcpForwarding (and others) will allow
+##   opening forwarded connections prior to passing the second auth factor.
+##   On Linux, this can be circumvented with PAM,
+##   I have yet to find an equivalent solution for OpenBSD.
+##
+
+# gen_sshd_config_base ( allow_groups )
+gen_sshd_config_base() {
+    local iter
+
+cat << EOF
+HostKeyAlgorithms ssh-ed25519,rsa-sha2-512
+MACs hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com
+KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512
+Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com
+
+EOF
+
+for iter in ${SSHD_HOST_KEY_TYPES:?}; do
+    printf 'HostKey %s/ssh_host_%s_key\n' "${SSHD_CONFDIR}" "${iter}"
+done
+
+cat << EOF
+
+AllowGroups ${OCONF_SSHD_GROUP_LOGIN:?}
+PermitRootLogin no
+
+AuthenticationMethods publickey
+PasswordAuthentication no
+ChallengeResponseAuthentication no
+PubkeyAuthentication yes
+
+AuthorizedKeysFile	${SSHD_SYSTEM_AUTH_KEYS_DIR}/%u
+
+GatewayPorts no
+PermitTunnel no
+
+AllowAgentForwarding no
+AllowTcpForwarding no
+AllowStreamLocalForwarding no
+GatewayPorts no
+X11Forwarding no
+PermitTTY no
+PermitUserEnvironment no
+PermitUserRC no
+
+Banner none
+PrintLastLog no
+PrintMotd no
+
+TCPKeepAlive yes
+UseDNS no
+
+Subsystem sftp internal-sftp
+EOF
+
+if [ -n "${OCONF_SSHD_GROUP_SHELL-}" ]; then
+cat << EOF
+
+Match Group ${OCONF_SSHD_GROUP_SHELL}
+    PermitTTY yes
+EOF
+fi
+
+if [ -n "${OCONF_SSHD_GROUP_FORWARDING-}" ]; then
+cat << EOF
+
+Match Group ${OCONF_SSHD_GROUP_FORWARDING}
+    AllowTcpForwarding yes
+EOF
+fi
+}
--- site/src/rootfs/base/FACTORY/site/share/sshd/setup.sh
+++ site/src/rootfs/base/FACTORY/site/share/sshd/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# sshd_setup ( login_users, ctrl_users, jump_users )
+# sshd_setup()
 #
 sshd_setup() {
     dodir_mode "${SSHD_CONFDIR}" 0755 'root:wheel' || return
     dodir_mode "${SSHD_SYSTEM_AUTH_KEYS_DIR}" 0711 'root:wheel' || return
-    sshd_setup_create_sshd_config "${@}" || return
+    sshd_setup_create_sshd_config || return
     sshd_setup_create_host_keys || return
     sshd_setup_disable_rc_keygen || return
 }
@@ -39,31 +39,13 @@ sshd_setup_create_host_keys() {
 }
 
 
-# _sshd_setup_gen_sshd_config ( allow_users, login_users, ctrl_users, jump_users )
+# _sshd_setup_gen_sshd_config()
 #
-#  Simple gen_sshd_config() wrapper that supports the various user types
+#  Simple gen_sshd_config() wrapper that supports the various access groups
 #  but does not allow any further per-user customization.
 #
-#  All users must be listed in the "allow_users" list,
-#  and also in one of the "{login,ctrl,jump}_users" lists.
-#
 _sshd_setup_gen_sshd_config() {
-    local user
-
-    [ -n "${1-}" ] || return
-    gen_sshd_config_base "${1}" || return
-
-    for user in ${2-}; do
-        gen_sshd_login_user "${user}" || return
-    done
-
-    for user in ${3-}; do
-        gen_sshd_ctrl_user "${user}" || return
-    done
-
-    for user in ${4-}; do
-        gen_sshd_jump_user "${user}" || return
-    done
+    gen_sshd_config_base || return
 }
 
 sshd_setup_create_sshd_config() {
