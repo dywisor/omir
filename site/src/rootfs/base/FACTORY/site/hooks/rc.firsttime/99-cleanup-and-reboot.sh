cd23d2225291f52b7bd2d422a552c64ee2233a59
André Erdmann <dywi@mailerd.de>
2021-04-17 16:52:57 +0200

    site: refactor cleanup code
    prepare for http-reporting after cleanup/reboot

--- site/src/rootfs/base/FACTORY/site/hooks/rc.firsttime/99-cleanup-and-reboot.sh
+++ site/src/rootfs/base/FACTORY/site/hooks/rc.firsttime/99-cleanup-and-reboot.sh
@@ -15,12 +15,15 @@ else
 fi
 
 run_cleanup_code=0
+run_report_code=0
 will_reboot=0
-cleanup_code="
+_script_header="
 #!/bin/sh
 set -fu
 cd /
 "
+cleanup_code="${_script_header}"
+report_code="${_script_header}"
 
 add_cleanup_code() {
 
@@ -74,14 +77,17 @@ cleanup_code="${cleanup_code}
 }
 
 
-[ "${OFEAT_AUTO_CLEANUP_FILES:-0}" -eq 0 ] || add_cleanup_code
+if feat_all "${OFEAT_AUTO_CLEANUP_FILES-}"; then
+    add_cleanup_code
+fi
 
-print_action "Cleanup"
+print_action "Cleanup and reporting"
 
-if [ "${OFEAT_AUTO_REBOOT:-0}" -eq 1 ] && [ -e "${AUTO_REBOOT_FLAG_FILE}" ]; then
+if feat_all "${OFEAT_AUTO_REBOOT-}" && [ -e "${AUTO_REBOOT_FLAG_FILE}" ]; then
 	if ! rm -- "${AUTO_REBOOT_FLAG_FILE}"; then
 		print_err "Could not remove auto-reboot flag file, will not reboot."
-	elif [ "${OFEAT_AUTO_REBOOT_DELAY:-0}" -eq 1 ]; then
+
+    elif feat_all "${OFEAT_AUTO_REBOOT_DELAY-}"; then
 		print_info "Adding delayed reboot code"
 		add_delayed_reboot_code "${OCONF_AUTO_REBOOT_DELAY-}"
 	else
@@ -90,23 +96,33 @@ if [ "${OFEAT_AUTO_REBOOT:-0}" -eq 1 ] && [ -e "${AUTO_REBOOT_FLAG_FILE}" ]; the
 	fi
 fi
 
+if feat_all "${OFEAT_FLAG_FILE_INSTALLED-}"; then
+    run_report_code=1
+    report_code="${report_code}${NL}touch -- /OMIR_INSTALLED${NL}"
+fi
 
-if [ ${run_cleanup_code} -eq 1 ]; then
-    if [ "${OFEAT_FLAG_FILE_INSTALLED:-0}" -eq 1 ]; then
+# attach or run report code
+if [ ${run_report_code} -eq 1 ]; then
+    if [ ${run_cleanup_code} -eq 1 ]; then
         if [ ${will_reboot} -eq 1 ]; then
+            # install report code as next rc.firsttime script
             dofile_site /etc/rc.firsttime 0700 'root:wheel' \
-                printf '%s\n' '#!/bin/sh' 'touch -- /OMIR_INSTALLED'
-        else
-            cleanup_code="${cleanup_code}${NL}touch -- /OMIR_INSTALLED${NL}"
-        fi
-    fi
+                printf '%s\n' "${report_code}"
 
+        else
+            # attach report code to cleanup code
+            cleanup_code="${cleanup_code}${NL}${report_code}"
+        fi
+
+    else
+        # run report code now
+        /bin/sh -c "${report_code}" || :
+    fi
+fi
+
+# run cleanup code
+if [ ${run_cleanup_code} -eq 1 ]; then
 	print_info "Running cleanup code"
 	sync
 	exec /bin/sh -c "${cleanup_code}"
-
-else
-    if [ "${OFEAT_FLAG_FILE_INSTALLED:-0}" -eq 1 ]; then
-        touch -- /OMIR_INSTALLED
-    fi
 fi
